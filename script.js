555df77db4612aa9383e9e0af07af03c40424c6b
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-04 21:51:30 -0400

    removed difficult set timer and defined functions

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-var ezBtn = document.getElementById("ezBtn");
-var medBtn = document.getElementById("medBtn");
-var hardBtn = document.getElementById("hardBtn");
 var startBtn = document.getElementById("submit");
+var timer = document.getElementById("timer");
+var startTime = 300; 
+
+startBtn.addEventListener('click', function(event){
+    event.preventDefault();
+
+var quizInterval = setInterval( function(){
+timer.textContent = startTime;
+startTime --;
+Quiz();
+
+if (startTime === 0){
+    clearInterval(quizInterval);
+    reportScore();
+}
+},1000)
+
+})
+function Quiz() {
+    startBtn.remove();
+    
+    console.log("quiz");
+}
+function reportScore(){
+
+    console.log("score")
+}
