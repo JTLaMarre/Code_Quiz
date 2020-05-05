e19dcad5f4793416ab4421e190fd7e5f33677e27
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-04 21:19:09 -0400

    set up files and doms

---
+++ script.js
@@ -0,0 +1,4 @@
+var ezBtn = document.getElementById("ezBtn");
+var medBtn = document.getElementById("medBtn");
+var hardBtn = document.getElementById("hardBtn");
+var startBtn = document.getElementById("submit");
